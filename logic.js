047e6a647514650dc3cc63a20a4916595bc9536b
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-09-06 11:31:10 -0400

    Created the TableBody function

--- logic.js
+++ logic.js
@@ -3,31 +3,35 @@ let BASE_URL = "https://api.coingecko.com/api/v3";
 let MARKET_DATA_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 let marketUrl = BASE_URL + MARKET_DATA_ENDPOINT;
 
-
-
-function generateTableBody(table, data) {
-  data.forEach(
-    $("<tr></tr").html;
-    $("<td>").text(data.market_cap_rank)
-    $("<td>").text(data.name)
-    $("<td>").text(data.market_cap)
-    $("<td>").text(data.current_price)
-    $("<td>").text(data.total_volume) //24h Volume
-    $("<td>").text(data.circulating_supply)
-    $("<td>").text(data.market_cap_change_24h)
-  )
+function generateTableBody(data) {
+  $("#coinTableBody") = ""; // remove previous data
+  for (let key in data) {
+    $("#coinTableBody").append(
+      $("<tr class = "content-row"></tr>").append(
+        $("<td class = "text-left"></td>").text(data[key].market_cap_rank),
+        $("<td class = "text-left"></td>").text(data[key].name),
+        $("<td class = "text-right"></td>").text(data[key].market_cap),
+        $("<td class = "text-right"></td>").text(data[key].current_price),
+        $("<td class = "text-right"></td>").text(data[key].total_volume),
+        $("<td class = "text-right"></td>").text(data[key].circulating_supply),
+        $("<td class = "text-right"></td>").text(data[key].market_cap_change_24h)
+      )
+    );
+  };
 }
 
+function getApiData() {
+  $(document).ready(function() {
+    fetch(marketUrl)
+    .then( res => {
+      res.json().then( res => {
+        generateTableBody(res);
+      })
+  })
+  .catch( err => {
+    console.log(err);
+  });
+  });
+};
 
-
-$(document).ready(function() {
-  fetch(marketUrl)
-  .then( res => {
-    res.json().then( res => {
-      generateTableBody($("#coinTable"), res)
-    })
-})
-.catch( err => {
-  console.log(err);
-});
-});
+getApiData();
