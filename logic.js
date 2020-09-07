9c5987eb5159ccc674c24374a80ff35d57118269
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-09-06 20:14:09 -0400

    fixed bugs / improved design

--- logic.js
+++ logic.js
@@ -4,17 +4,20 @@ let MARKET_DATA_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc
 let marketUrl = BASE_URL + MARKET_DATA_ENDPOINT;
 
 function generateTableBody(data) {
-  $("#coinTableBody") = ""; // remove previous data
   for (let key in data) {
-    $("#coinTableBody").append(
-      $("<tr class = "content-row"></tr>").append(
-        $("<td class = "text-left"></td>").text(data[key].market_cap_rank),
-        $("<td class = "text-left"></td>").text(data[key].name),
-        $("<td class = "text-right"></td>").text(data[key].market_cap),
-        $("<td class = "text-right"></td>").text(data[key].current_price),
-        $("<td class = "text-right"></td>").text(data[key].total_volume),
-        $("<td class = "text-right"></td>").text(data[key].circulating_supply),
-        $("<td class = "text-right"></td>").text(data[key].market_cap_change_24h)
+    $('#coinTableBody').append(
+      $('<tr class="content-row"></tr>').append(
+        $('<td class="text-center"></td>').text(data[key].market_cap_rank),
+        $('<td class="text-left"></td>').append(
+          $('<div></div>').append(
+            `<img src="${data[key].image}" width="16"> ${data[key].name}`
+            )
+          ),
+        $('<td class="text-right"></td>').text("$" + data[key].market_cap),
+        $('<td class="text-right"></td>').text("$" + data[key].current_price),
+        $('<td class="text-right"></td>').text("$" + data[key].total_volume),
+        $('<td class="text-right"></td>').text(data[key].circulating_supply.toFixed(0) + " " + data[key].symbol.toUpperCase()),
+        $(`<td class='${data[key].price_change_percentage_24h >= 0 ? "text-success" : "text-danger"} text-right'></td>`).text(data[key].market_cap_change_percentage_24h.toFixed(2) + "%")
       )
     );
   };
