029227e02d427d20cde696392241c66e5cd40a60
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-09-06 21:39:24 -0400

    Added navigation, fixed minor errors

--- logic.js
+++ logic.js
@@ -1,9 +1,12 @@
 ethereum.autoRefreshOnNetworkChange = false;
+const coinsPerPage = 100;
+const currentPage = 1;
 let BASE_URL = "https://api.coingecko.com/api/v3";
-let MARKET_DATA_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+let MARKET_DATA_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${coinsPerPage}&page=${currentPage}&sparkline=false";
 let marketUrl = BASE_URL + MARKET_DATA_ENDPOINT;
 
 function generateTableBody(data) {
+  let number = Intl.NumberFormat("en-US");
   for (let key in data) {
     $('#coinTableBody').append(
       $('<tr class="content-row"></tr>').append(
@@ -13,11 +16,11 @@ function generateTableBody(data) {
             `<img src="${data[key].image}" width="16"> ${data[key].name}`
             )
           ),
-        $('<td class="text-right"></td>').text("$" + data[key].market_cap),
-        $('<td class="text-right"></td>').text("$" + data[key].current_price),
-        $('<td class="text-right"></td>').text("$" + data[key].total_volume),
-        $('<td class="text-right"></td>').text(data[key].circulating_supply.toFixed(0) + " " + data[key].symbol.toUpperCase()),
-        $(`<td class='${data[key].price_change_percentage_24h >= 0 ? "text-success" : "text-danger"} text-right'></td>`).text(data[key].market_cap_change_percentage_24h.toFixed(2) + "%")
+        $('<td class="text-right"></td>').text("$" + number.format(data[key].market_cap)),
+        $('<td class="text-right"></td>').text("$" + number.format(data[key].current_price)),
+        $('<td class="text-right"></td>').text("$" + number.format(data[key].total_volume)),
+        $('<td class="text-right"></td>').text(number.format(data[key].circulating_supply.toFixed()) + " " + data[key].symbol.toUpperCase()),
+        $(`<td class='${data[key].price_change_percentage_24h >= 0 ? "text-success" : "text-danger"} text-right'></td>`).text(Number(data[key].market_cap_change_percentage_24h).toFixed(2) + "%")
       )
     );
   };
@@ -37,4 +40,9 @@ function getApiData() {
   });
 };
 
-getApiData();
+async function refreshTableBody() {
+  let content = await getApiData();
+  generateTableBody(content);
+}
+
+refreshTableBody();
